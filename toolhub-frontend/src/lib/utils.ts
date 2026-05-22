2d17690cc338b49b14cf9bd341730475822d78ac
TheBandik <shnr.arky@gmail.com>
2026-05-22 11:22:47 +1000

    add shadcn components: card, input, label, dialog, dropdown-menu, badge, table

--- toolhub-frontend/src/lib/utils.ts
+++ toolhub-frontend/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export type WithoutChildren<T> = Omit<T, 'children'>;
+export type WithoutChild<T> = Omit<T, 'child'>;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
+	ref?: U | null;
+};
